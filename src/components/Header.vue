c55b5c1ae98f655ed15256dfb53ba2c836db938b
Jason <jason.lloyd.law@gmail.com>
2020-07-04 09:30:31 +1000

    updated icons and logos

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="w-full flex justify-between items-center sticky top-0 header-fade h-16 -mb-16 px-2 sm:px-4 xl:px-20">
 		
-		<router-link to="/"><h1 class="text-4xl">KingKai</h1></router-link>
+		<router-link to="/"><img src="/img/icons/Logo-sm.png" alt=""></router-link>
         <div class="w-full flex justify-end items-center" id="nav">
 			<router-link class="mr-4 sm:mr-8" to="/posts">Memories</router-link>
 			<router-link class="mr-4 sm:mr-8" to="/about">About</router-link>
